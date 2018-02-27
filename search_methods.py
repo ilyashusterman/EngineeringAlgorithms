d4369a69a2e11530e37c7c98e2987f467a596a5e
Ilya Shusterman <ilya@allaspect.com>
2018-02-27 13:17:48 +0200

    added 2 tests

--- search_methods.py
+++ search_methods.py
@@ -2,7 +2,8 @@ from unittest import TestCase
 
 
 def linear_search(sequence, target):
-    # value_found = filter(lambda x: x == target, sequence)
+    """Algorithm 	Best case 	Expected 	Worst case"""
+    """Linear search 	O(1) 	O(N) 	O(N)"""
     for index, item in enumerate(sequence):
         if item == target:
             return index
@@ -12,19 +13,24 @@ def linear_search(sequence, target):
         return None
     else:
         return indexes[0]
+    # value_found = filter(lambda x: x == target, sequence)
+    # Second slower option
 
 
-def binary_search(sorted_collection, item):
+def binary_search(sorted_collection, search_value):
+    """Algorithm 	Best case 	Expected 	Worst case"""
+    """Binary search 	O(1) 	O(log N) 	O(log N)"""
     left = 0
     right = len(sorted_collection) - 1
 
     while left <= right:
         midpoint = (left + right) // 2
-        current_item = sorted_collection[midpoint]
-        if current_item == item:
+        current_value = sorted_collection[midpoint]
+        if current_value == search_value:
             return midpoint
         else:
-            if item < current_item:
+
+            if search_value < current_value:
                 right = midpoint - 1
             else:
                 left = midpoint + 1
@@ -35,14 +41,13 @@ def binary_search(sorted_collection, item):
 class TestSearchAlgorithms(TestCase):
 
     def test_linear_search(self):
-        value_found = linear_search([0, 5, 7, 10, 15], 0)
-        self.assertEqual(value_found, 0)
-        value_found = linear_search([0, 5, 7, 10, 15], 5)
-        self.assertEqual(value_found, 1)
+        index_found = linear_search([0, 5, 7, 10, 15], 0)
+        self.assertEqual(index_found, 0)
+        index_found = linear_search([0, 5, 7, 10, 15], 5)
+        self.assertEqual(index_found, 1)
 
     def test_binary_search(self):
-        pass
-
-if __name__ == '__main__':
-    print('Using linear search:={}'.format(
-        linear_search([0, 5, 7, 10, 15], 0)))
+        index_found = binary_search([0, 5, 7, 10, 15], 15)
+        self.assertEqual(index_found, 4)
+        none_value =  binary_search([0, 5, 7, 10, 15], 6)
+        self.assertIsNone(none_value)
